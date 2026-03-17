eabefb8361d2a2a8d509d16d0f7108c7d11c5d18
ngxbe <ngxbeast@gmail.com>
2026-03-17 06:29:12 -0400

    going back

---
+++ config/corne-layers.h
@@ -0,0 +1,34 @@
+// Layer and color defines for ZMK firmware (compatible with device tree and C)
+// Include this file in your .dtsi or C files as needed
+
+#define LAYER_ABC 0
+#define LAYER_NMRW 1
+#define LAYER_KPAD 2
+#define LAYER_XTRA 3
+#define LAYER_XTR2 4
+#define LAYER_GAME 5
+#define LAYER_GFUN 6
+#define LAYER_GAMETGL 7
+#define LAYER_GAME2 8
+#define LAYER_GAME2_SUBLR1 9
+#define LAYER_TGL 10
+#define LAYER_TGLLR_1 11
+#define LAYER_TGLLR_1_SUBLR_1 12
+#define LAYER_TGLLR_2 13
+#define LAYER_TGLLR_2_SUBLR_1 14
+
+#define RGB_ABC         RGB_COLOR_HSB(19,100,17)
+#define RGB_NMRW        RGB_COLOR_HSB(183,45,18)
+#define RGB_KPAD        RGB_COLOR_HSB(230,87,26)
+#define RGB_XTRA        RGB_COLOR_HSB(273,67,23)
+#define RGB_GAME        RGB_COLOR_HSB(0,100,25)
+#define RGB_GAMETGL     RGB_COLOR_HSB(200,100,60)
+#define RGB_GAME2       RGB_COLOR_HSB(73,67,15)
+#define RGB_GFUN        RGB_COLOR_HSB(116,67,50)
+#define RGB_XTRA2       RGB_COLOR_HSB(116,67,26)
+#define RGB_TGL         RGB_COLOR_HSB(251,67,50)
+#define RGB_TGLLR_1     RGB_COLOR_HSB(40,100,12)
+#define RGB_TGLLR_2     RGB_COLOR_HSB(85,100,15)
+#define RGB_TGLLR_2_SUBLR_1 RGB_COLOR_HSB(82,100,50)
+#define RGB_TGLLR_1_SUBLR_1 RGB_COLOR_HSB(58,45,50)
+#define RGB_GAME2_SUBLLR_1 RGB_COLOR_HSB(67,55,50)
